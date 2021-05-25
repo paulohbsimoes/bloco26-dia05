ced5f2d25363300b1e4f2d9005f97c0514778beb
Rosiele David <rosieleserpa@hotmail.com>
2021-05-24 22:28:02 -0300

    Atividade 10 concluída

--- routes/recipe.js
+++ routes/recipe.js
@@ -4,6 +4,12 @@ const middleware = require('../middlewares');
 
 route.delete('/:id', middleware.recipe.deleteById);
 
+route.put('/:id/ingredients', [
+  middleware.recipe.verifyId,
+  middleware.recipe.deleteIngredients,
+  middleware.recipe.addIngredients,
+]);
+
 route.put('/:id', middleware.recipe.updateById);
 
 module.exports = route;
